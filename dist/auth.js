709ca887f6cd96dc6c69ff46349d686ed5f0ee65
Chris Hannaby <chris.hannaby@netlify.com>
2022-07-26 08:05:41 -0600

    add id to login page

--- dist/auth.js
+++ dist/auth.js
@@ -1,24 +1,27 @@
 netlifyIdentity.on("login", (user) => {
-  const url = new URL(document.location.href);
-  // Add query param to refresh page on login for auth’d content
-  if (url.searchParams.get("refresh") !== "login") {
-    window.netlifyIdentity
-      .refresh()
-      .then((jwt) => {
-        url.searchParams.set("refresh", "login");
-        url.pathname =
-          `/restricted/${url.searchParams.get("return_url")}` || "/restricted";
-        url.searchParams.delete("return_url");
-        document.location.href = url.toString();
-      })
-      .catch((err) => {
-        console.log(
-          "Identity Error: Unable to get new valid token. logging out user",
-          err
-        );
-        clearUser();
-        window.netlifyIdentity.open();
-      });
+  if (document.getElementById("netlify-identity-login-page")) {
+    const url = new URL(document.location.href);
+    // Add query param to refresh page on login for auth’d content
+    if (url.searchParams.get("refresh") !== "login") {
+      window.netlifyIdentity
+        .refresh()
+        .then((jwt) => {
+          url.searchParams.set("refresh", "login");
+          url.pathname =
+            `/restricted/${url.searchParams.get("return_url")}` ||
+            "/restricted";
+          url.searchParams.delete("return_url");
+          document.location.href = url.toString();
+        })
+        .catch((err) => {
+          console.log(
+            "Identity Error: Unable to get new valid token. logging out user",
+            err
+          );
+          clearUser();
+          window.netlifyIdentity.open();
+        });
+    }
   }
 });
 netlifyIdentity.on("logout", () => {
